8a2d3103b468e95f77f155fc7067bd9f2c5d2d0a
Ben Sarmiento <me@bensarmiento.com>
2016-02-24 04:22:41 +0800

    Monitor surge pricing code

--- content.js
+++ content.js
@@ -1,6 +1,16 @@
-document.onreadystatechange = function () {
-  if (document.readyState == "interactive") {
-    var el = document.querySelector("div.surge-data > p");
-    console.log(el);
+var onSurgePricing = -1;
+var surgePtn = /<i class="icon-surge"><\/i>(\d.\d)<small>x<\/small>/;
+
+var intervalID = window.setInterval(function() {
+  var el = document.querySelector("div.surge-data > p");
+  if (!!el) {
+    onSurgePricing = 1;
+  } else {
+    if (onSurgePricing === 1) {
+      alert("No more surge!");
+    }
+    onSurgePricing = 0;
   }
-}
+  console.log(onSurgePricing);
+  // surgePtn.exec(el.innerHTML)[1]
+}, 5000);
